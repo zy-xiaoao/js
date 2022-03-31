d83dacb0c92eeca5c61e1688dd10032aae288d51
zy-xiaoao <102721476+zy-xiaoao@users.noreply.github.com>
2022-03-31 17:00:52 +0800

    Create ssa.js

---
+++ ssa.js
@@ -0,0 +1,25 @@
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const vip = '/cms/Appapi/username/username';
+
+if (url.indexOf(vip) != -1) {
+
+    obj.point= 999999;
+
+    obj.jomkenylv = 99;
+
+    obj.groupname = "白金会员";
+
+    obj.overduedate = 2027383523;
+
+    obj.overduedate2 = 2027383523;
+
+    body = JSON.stringify(obj);
+
+}
+
+$done({body});
