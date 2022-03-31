0bf8ee4ac7c406907cd4b0b6513441f03afdea36
zy-xiaoao <102721476+zy-xiaoao@users.noreply.github.com>
2022-03-31 22:56:34 +0800

    Create meituxiuxiu.js

---
+++ meituxiuxiu.js
@@ -0,0 +1,21 @@
+‼️规则完全免费，仅供学习交流，🈲️商业用途
+
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const vip = '/v1/vip';
+
+if (url.indexOf(vip) != -1) {
+
+    obj.data.vip_type = 1;
+
+    obj.data.is_valid_user = 1;
+
+    body = JSON.stringify(obj);
+
+}
+
+$done({body});
