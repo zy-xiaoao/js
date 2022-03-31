b499931004aad92a79a4fc33b1dfc79af6e64b80
zy-xiaoao <102721476+zy-xiaoao@users.noreply.github.com>
2022-03-31 20:58:06 +0800

    Create naiyou.js

---
+++ naiyou.js
@@ -0,0 +1,20 @@
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const vip = '/account/me';
+
+if (url.indexOf(vip) != -1) {
+
+    obj.data.vip = true;
+
+    obj.data.createAt = "2099-09-09 09:09:09"
+
+    body = JSON.stringify(obj);
+
+}
+
+$done({body});
+
