f5b16fddb0003853bd84ba5fa0ca64050ed3caae
zy-xiaoao <102721476+zy-xiaoao@users.noreply.github.com>
2023-12-21 15:29:17 +0800

    Update xinxi.js

--- xinxi.js
+++ xinxi.js
@@ -1,12 +1,6 @@
-var body = $response.body;
 var url = $request.url;
-var obj = JSON.parse(body);
-
+var hd = $request.header;
 const vip = '/GetOpenId';
-
 if (url.indexOf(vip) != -1) {
-    obj.data.IsAuth= "1";
-    body = JSON.stringify(obj);
+    hd.Authorization= "Bearer eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6NzA4NDB9.VWRNwCYAr9PSYHXs1PP1_bszTgl29Oz6KuJO-XunXm8flcrIJXLa8LqfVcYu97y_jqYCh8XyEEnLOP8GEUW3JQ"
 }
-
-$done({body});
